03053b94f84ed68e636e52c3320c45db91b50fe4
Máté Steffler <mate.steffler@stud.uni-corvinus.hu>
2021-09-30 10:52:03 +0100

    5 rész

--- IRF_3.Gyak3.0/Form1.cs
+++ IRF_3.Gyak3.0/Form1.cs
@@ -15,6 +15,9 @@ namespace IRF_3.Gyak3._0
         public Form1()
         {
             InitializeComponent();
+            label1.Text = Resource1.LastName;
+            label2.Text = Resource1.FirstName;
+            button1.Text = Resource1.Add;
         }
     }
 }
