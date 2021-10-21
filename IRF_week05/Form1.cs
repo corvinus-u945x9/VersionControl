b35bfb9344e77f4ea73c4c96840d7a74f3c2fa4e
Máté Steffler <mate.steffler@stud.uni-corvinus.hu>
2021-10-21 09:54:16 +0100

    8

--- IRF_week05/Form1.cs
+++ IRF_week05/Form1.cs
@@ -21,6 +21,24 @@ namespace IRF_week05
             ticks = context.Ticks.ToList();
             dataGridView1.DataSource = ticks;
             CreatPortfolio();
+            List<decimal> Nyereségek = new List<decimal>();
+            int intervalum = 30;
+            DateTime kezdőDátum = (from x in ticks select x.TradingDay).Min();
+            DateTime záróDátum = new DateTime(2016, 12, 30);
+            TimeSpan z = záróDátum - kezdőDátum;
+            for (int i = 0; i < z.Days - intervalum; i++)
+            {
+                decimal ny = GetPortfolioValue(kezdőDátum.AddDays(i + intervalum))
+                           - GetPortfolioValue(kezdőDátum.AddDays(i));
+                Nyereségek.Add(ny);
+                Console.WriteLine(i + " " + ny);
+            }
+
+            var nyereségekRendezve = (from x in Nyereségek
+                                      orderby x
+                                      select x)
+                                        .ToList();
+            MessageBox.Show(nyereségekRendezve[nyereségekRendezve.Count() / 5].ToString());
         }
 
         private void CreatPortfolio()
