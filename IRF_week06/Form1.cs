164cafc6e6a95ad10d8a80add719bc428b4b3604
Máté Steffler <mate.steffler@stud.uni-corvinus.hu>
2021-10-28 13:14:47 +0100

    5

--- IRF_week06/Form1.cs
+++ IRF_week06/Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace IRF_week06
 {
@@ -23,11 +24,12 @@ namespace IRF_week06
             dataGridView1.DataSource = rates;
         }
 
+
         private void Fügvény()
         {
-            var mnbService = new MNBArfolyamServiceSoapClient();
+             var mnbService = new MNBArfolyamServiceSoapClient();
 
-            var request = new GetExchangeRatesRequestBody()
+             var request = new GetExchangeRatesRequestBody()
             {
                 currencyNames = "EUR",
                 startDate = "2020-01-01",
@@ -36,6 +38,26 @@ namespace IRF_week06
 
             var response = mnbService.GetExchangeRates(request);
             var result = response.GetExchangeRatesResult;
+
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                
+                var rate = new RateDate();
+                rates.Add(rate);
+
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
         }
     }
 }
