e341cf43aba8b794258ee537513e835c4c88fa81
Máté Steffler <mate.steffler@stud.uni-corvinus.hu>
2021-10-28 13:20:33 +0100

    6

--- IRF_week06/Form1.cs
+++ IRF_week06/Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 namespace IRF_week06
@@ -22,6 +23,7 @@ namespace IRF_week06
             InitializeComponent();
             Fügvény();
             dataGridView1.DataSource = rates;
+            
         }
 
 
@@ -58,6 +60,22 @@ namespace IRF_week06
                 if (unit != 0)
                     rate.Value = value / unit;
             }
+
+            chartRateData.DataSource = rates;
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+
         }
     }
 }
