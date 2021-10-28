a8eaa0ebf4f6f222ff11b249c02037434fc2d346
Máté Steffler <mate.steffler@stud.uni-corvinus.hu>
2021-10-28 12:56:30 +0100

    3

--- IRF_week06/Form1.cs
+++ IRF_week06/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IRF_week06.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,26 @@ namespace IRF_week06
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
+            Fügvény();
+        }
+
+        private void Fügvény()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
