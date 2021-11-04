638b4cdbaac8341c0e0008c7cc9c85d550452e00
Máté Steffler <mate.steffler@stud.uni-corvinus.hu>
2021-11-04 10:16:09 +0100

    CarFactor

--- IRF_week07/Abstractions/IToyFactory.cs
+++ IRF_week07/Abstractions/IToyFactory.cs
@@ -6,11 +6,8 @@ using System.Threading.Tasks;
 
 namespace IRF_week07
 {
-    public class BallFactory:IToyFactory
+    public interface IToyFactory
     {
-        public Toy CreateNew()
-        {
-            return new Ball();
-        }
+        Toy CreateNew();
     }
 }
--- IRF_week07/Entities/CarFactory.cs
+++ IRF_week07/Entities/CarFactory.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace IRF_week07
 {
-    public class BallFactory:IToyFactory
+    class CarFactory:IToyFactory
     {
         public Toy CreateNew()
         {
-            return new Ball();
+            return new Car();
         }
     }
 }
