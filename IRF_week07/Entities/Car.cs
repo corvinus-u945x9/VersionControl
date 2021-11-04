fcacd7b323772fd08770f29f526147fd39e1ac75
Máté Steffler <mate.steffler@stud.uni-corvinus.hu>
2021-11-04 10:08:12 +0100

    CAR

--- IRF_week07/Entities/Car.cs
+++ IRF_week07/Entities/Car.cs
@@ -4,16 +4,15 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace IRF_week07
 {
-    public class Ball:Toy
+    class Car : Toy
     {
         protected override void DrawImage(Graphics g)
         {
-            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
         }
-
     }
 }
